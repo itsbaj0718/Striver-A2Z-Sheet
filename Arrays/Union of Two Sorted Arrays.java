54c18223db53fb80a66f1e31fc077ad9bc01cb52
Bajrang Yadav <125151953+itsbaj0718@users.noreply.github.com>
2023-07-07 13:26:11 +0530

    Create Union of Two Sorted Arrays.java

---
+++ Arrays/Union of Two Sorted Arrays.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<n;i++)
+         set.add(arr1[i]);
+         
+        for(int i=0;i<m;i++)
+          set.add(arr2[i]);
+          
+        ArrayList<Integer> arr=new ArrayList<>(set);
+        
+        Collections.sort(arr);
+        return arr;
+        
+    }
+}
